756c0e4158e91f22f9b9d2274f2778f86e5deccc
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2024-02-02 16:44:24 +0530

    fix:fixed

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,31 @@
-export const pasientLoging = (req, res) => {
-  const { name, email, number } = req.body;
+import Patient from "../model/patientModule";
+
+export const pasientLoging = async (req, res) => {
   try {
-  } catch (error) {}
+    const { username, email, number } = req.body;
+    const existingUser = await Patient.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (existingUser) {
+      res.status(400).json("User alredy exist");
+    }
+
+    const newUser = new Patient({
+      name,
+      email,
+      username,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    res.status(200).json({
+        username:newUser.name,
+        email:newUser.email,
+        number:newUser.number
+    })
+  } catch (error) {
+    console.log("error",error)
+  }
 };
--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const   Router  = require("./route/pasientRoute");
 dotenv.config();
 
 // mongoose
 //   .connect(process.env.MONGO_URL)
 //   .then(() => console.log("connection is good"));
- 
+
+app.use("/api/patients",Router);
+
 app.listen(process.env.PORT, () =>
   console.log(`port is running on ${process.env.PORT}`)
 );
- 
